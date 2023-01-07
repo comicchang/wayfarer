f7dae82e962785030c7400de1385adc17e04217e
iroken22 <compunerd451@gmail.com>
2023-01-06 21:35:13 -0600

    Tweak - Incremented version number

--- wayfarer-planner.user.js
+++ wayfarer-planner.user.js
@@ -2,7 +2,7 @@
 // @id           wayfarer-planner@alfonsoml
 // @name         IITC plugin: Wayfarer Planner
 // @category     Layer
-// @version      1.162
+// @version      1.170
 // @namespace    https://gitlab.com/AlfonsoML/wayfarer/
 // @downloadURL  https://gitlab.com/AlfonsoML/wayfarer/raw/master/wayfarer-planner.user.js
 // @homepageURL  https://gitlab.com/AlfonsoML/wayfarer/
